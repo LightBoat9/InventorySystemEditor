43b83167bd033655859e70a27c5024e787ef5b01
Luke <gamersloop@gmail.com>
2018-02-22 19:21:07 -0500

    Add drag and drop

--- addons/inventory/types/inventory_item.gd
+++ addons/inventory/types/inventory_item.gd
@@ -1,42 +1,58 @@
 tool
 extends Sprite
-
-var draggable = true
-
+	
+export var draggable = true
+	
+onready var initial_parent = get_parent()
+	
 var _mouse_over = false
 var _dragging = false
-
-func _enter_tree():
-	centered = false
+	
+var slot = null
 	
 func _ready():
 	set_process_input(true)
+	set_physics_process(true)
 	
 func _is_point_inside_sprite(p):
 	var a = global_position
 	var b = Vector2(global_position.x + texture.get_size().x, global_position.y)
-	var c = Vector2(global_position.x, global_position.y + texture.get_size().y)
-	var d = Vector2(global_position.x + texture.get_size().x, global_position.y + texture.get_size().y)
+	var c = Vector2(global_position.x + texture.get_size().x, global_position.y + texture.get_size().y)
 	
-	var a1 = abs((a.x * (b.y - p.y) + b.x * (p.y - a.y) + p.x * (a.y - b.y)) / 2.0)
-	var a2 = abs((b.x * (c.y - p.y) + c.x * (p.y - b.y) + p.x * (b.y - c.y)) / 2.0)
-	var a3 = abs((c.x * (d.y - p.y) + d.x * (p.y - c.y) + p.x * (c.y - d.y)) / 2.0)
-	var a4 = abs((d.x * (a.y - p.y) + a.x * (p.y - d.y) + p.x * (d.y - a.y)) / 2.0)
-	
-	if a1 == 0 or a2 == 0 or a3 == 0 or a4 == 0:
+	if p.x >= a.x and p.x <= b.x and p.y >= a.y and p.y <= c.y:
 		return true
-	
-	var sum = a1 + a2 + a3 + a4
-	var rec_area = abs((a.x * b.y) + (b.x * c.y) + (c.x * d.y) + (d.x * a.y) - (b.x * a.y) - (c.x * b.y) - (d.x * c.y) - (a.x * d.y)) / 2.0
-	var dum_area = texture.get_size().x * texture.get_size().y
-	
-	print(sum, " ", rec_area)
-	
-	if sum == rec_area:
-		return true
-		
-	return false
+	else:
+		return false
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		_is_point_inside_sprite(event.global_position)
+		_mouse_over = _is_point_inside_sprite(event.global_position)
+	elif event is InputEventMouseButton: 
+		if event.button_index == BUTTON_LEFT:
+			if _mouse_over and event.pressed and draggable:
+				_dragging = true
+				if slot:
+					slot = null
+					scale = Vector2(1,1)
+					get_parent().remove_child(self)
+					initial_parent.add_child(self)
+			elif not event.pressed:
+				_dragging = false
+				_drop()
+	
+func _physics_process(delta):
+	if _dragging:
+		global_position = get_viewport().get_mouse_position()
+		if not centered:
+			global_position -= texture.get_size() / 2.0
+	
+func _drop():
+	var p = get_viewport().get_mouse_position()
+	
+	for slot in get_tree().get_nodes_in_group("inventory_slots"):
+		var a = slot.global_position
+		var b = Vector2(slot.global_position.x + slot.texture.get_size().x, slot.global_position.y)
+		var c = Vector2(slot.global_position.x + slot.texture.get_size().x, slot.global_position.y + slot.texture.get_size().y)
+		if p.x >= a.x and p.x <= b.x and p.y >= a.y and p.y <= c.y:
+			slot.set_item(self)
+			break
--- addons/inventory/types/inventory_slot.gd
+++ addons/inventory/types/inventory_slot.gd
@@ -3,14 +3,24 @@ extends Sprite
 
 var InventoryItem = load("res://addons/inventory/types/inventory_item.gd")
 
+export var scale_item = false
+
+var item = null
+
 func _enter_tree():
+	add_to_group("inventory_slots")
 	texture = load("res://addons/inventory/assets/slot.png")
 	centered = false
-	
-	var item = InventoryItem.new()
-	item.texture = load("res://addons/inventory/assets/sword.png")
+	set_process(true)
 	
 func set_item(item):
-	if item:
-		item.queue_free()
+	self.item = item
+	item.position = Vector2()
+	item.z_index = z_index + 1
+	
+	if scale_item:
+		item.scale = texture.get_size() / item.texture.get_size()
+	
+	item.slot = self
+	item.get_parent().remove_child(item)
 	add_child(item)
